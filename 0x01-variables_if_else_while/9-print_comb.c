f602eb466d91a945a73e88f9fb3ff5e29fa955c5
Ocholla <ocholaesther1@gmail.com>
2023-06-16 14:23:06 -0700

    adding files

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - code execution starts
+ *
+ * Return: always 0 by default
+ */
+
+int main(void)
+{
+	for (int n=48;n<58;n++)
+	{
+		putchar(n);
+		if (n!=57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+The files in this folder are:
+9-print_comb.c -  prints all possible combinations of single-digit numbers.
