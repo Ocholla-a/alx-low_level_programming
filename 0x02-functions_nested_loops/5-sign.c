2e32b2f34a6fbd56d1104d7851c92c462c60a387
Ocholla <ocholaesther1@gmail.com>
2023-06-20 12:52:05 -0700

    adding files

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	if (n < 0)
+	else (n < 0)
 	{
 		_putchar('-');
 		return (-1);
