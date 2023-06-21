d4b5baa31c2593610b7336c4bc09187d72fb8800
Ocholla <ocholaesther1@gmail.com>
2023-06-21 10:56:57 -0700

    adding files

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabet(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+int add(int, int);
+void times_table(void);
+void print_to_98(int n);
+void print_alphabet_x10(void);
+void print_no(int n);
