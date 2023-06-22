0184b7a9eb503bdf03cdade606a5b78bd6d5b77f
Ocholla <ocholaesther1@gmail.com>
2023-06-22 02:54:26 -0700

    adding files

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+/**
+ * main - code execution starts
+ *
+ * Return: 0 always
+ */
+
+void print_no(int n);
+
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i < 101 ; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) != 0))
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		if (((i % 5) == 0) && ((i % 3) != 0))
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		if (((i % 5) != 0) && ((i % 3) != 0))
+		{
+			print_no(i);
+			_putchar(' ');
+		}
+		return (0);
+	}
+}
+
+/**
+ * print_no - prints an integer passed to it
+ *@n: the integer to be printed
+ * Return: void
+ */
+
+void print_no(int n)
+{
+	if (n < 10)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_no((n) / 10);
+	_putchar('0' + (n) % 10);
+}
