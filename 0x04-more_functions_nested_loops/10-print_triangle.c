506916edb9c9e9b5c28f4e4c9ba11855d7a46726
Ocholla <ocholaesther1@gmail.com>
2023-06-22 05:54:30 -0700

    adding files

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,18 +9,18 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if ((size < 0) || (size == 0))
 		_putchar('\n');
 	else
 		for (i = 0 ; i < size ; i++)
 		{
-			for (j = (size - 1) ; j > 0 ; j--)
+			for (j = (size - 1) ; j > i ; j--)
 			{
 				_putchar(' ');
 			}
-			for (j = 0 ; j <= i ; j++)
+			for (k = 0 ; k <= i ; k++)
 			{
 				_putchar('#');
 			}
