8e5bcbbde34c21fea6856d2db13487aaf0320adf
Ocholla <ocholaesther1@gmail.com>
2023-06-22 07:33:59 -0700

    update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,3 +19,21 @@ void more_numbers(void)
 		_putchar('\n');
 	}
 }
+
+/**
+ * print_no - prints an integer passed to it
+ *@n: the integer to be printed
+ * Return: void
+ */
+
+void print_no(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_no((n) / 10);
+	_putchar('0' + ((n) % 10));
+}
