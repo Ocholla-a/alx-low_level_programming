c6f4a1dfc3c7de5ff7ddcc8a97b22be7a20ab4a7
Ocholla <ocholaesther1@gmail.com>
2023-06-22 06:20:16 -0700

    adding files

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,8 +17,11 @@ void print_diagonal(int n)
 	{
 		for (i = 0 ; i < n ; i++)
 		{
-			for (j = 0 ; j < (n - 1) ; j++)
-				_putchar(92);
+			for (j = 0 ; j < i ; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
