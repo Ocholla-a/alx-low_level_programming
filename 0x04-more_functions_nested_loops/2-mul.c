38d526cd6160d1efc8539756801e219794cdd6a7
Ocholla <ocholaesther1@gmail.com>
2023-06-22 05:20:00 -0700

    adding files

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,13 @@
 
 /**
  * mul - multiplies two integers
+ * @a: first number
+ * @b: second number
  *
  * Return: the multiple of the two integers
  */
 
 int mul(int a, int b)
 {
-	return ((a)*(b));
+	return ((a) * (b));
 }
