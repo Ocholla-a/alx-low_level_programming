c6edb437f594ac08eda9eec7a86d8b6b0f959161
Ocholla <ocholaesther1@gmail.com>
2023-07-05 05:32:37 -0700

    adding files

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,11 +13,24 @@ void _puts_recursion(char *s)
 	int count, i;
 
 	count = strlen(s);
-	i = 0;
-	if (i < (count - 1))
-	{
-		_putchar(s[i]);
-		i++;
-	}
+	putsrecursion(0, count, s);
 	_putchar('\n');
 }
+
+/**
+ * putsrecursion - prints a string
+ * @i: flow
+ * @n: string length
+ * @s: the string
+ *
+ * Return: void
+ */
+
+void putsrecursion(int i, int n, char *s)
+{
+	if (i < n)
+	{
+		_putchar(s[i]);
+		putsrecursion(i+1, n, s);
+	}
+}	
