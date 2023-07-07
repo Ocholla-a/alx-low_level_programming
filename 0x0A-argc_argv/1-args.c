7e2cf928afd70927d3379bab4537fc82c26a5a5e
Ocholla <ocholaesther1@gmail.com>
2023-07-07 05:35:40 -0700

    adding files

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,8 +9,8 @@
  * Return: 0
  */
 
-int main(int argc, char __attribute__ ((unused)) **argv)
+int main(int argc, char __attribute__((unused)) **argv)
 {
-	printf ("%d\n", argc - 1);
-	return(0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
