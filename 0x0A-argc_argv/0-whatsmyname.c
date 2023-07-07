36c3fb772195ee48a5bc090aa125c653e8c1e050
Ocholla <ocholaesther1@gmail.com>
2023-07-07 05:28:03 -0700

    adding files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - code execution starts
+ * @argc: number of command arguments passed
+ * @argv: array of command arguments passed
+ * Return: 0
+ */
+
+int main(int argc __attribute__ ((unused)), char **argv)
+{
+	printf ("%s\n", argv[0]);
+	return (0);
+}
