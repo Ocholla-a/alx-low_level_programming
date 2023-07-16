c0ec3ca9dd16984bd972d6fbac98127d50597369
Ocholla <ocholaesther1@gmail.com>
2023-07-16 10:16:10 -0700

    adding files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strcat - concatenate two strings
+ * @dest: the first string
+ * @src: the second string
+ *
+ * Return: the resultant string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *s3 = strcat(dest, src);
+
+	return (s3);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,7 @@
+#include <string.h>
+#include "main.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	for (i = 0; s1[i] != '\0'; ++i)
+}
