aaca529a7527c7ad3894df67ef3464066e5c3a50
Ocholla <ocholaesther1@gmail.com>
2023-07-17 03:39:00 -0700

    adding README and dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defining structure of a dog
+ * @name: first property which is the name of the dog
+ * @owner: second property which is the name of the owner of the dog
+ * @age: third property which is the age of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
